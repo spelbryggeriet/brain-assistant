de4768f40a25ba4ff48550f99196a0a688d8599d
Hampus Lidin <hampuslidin@gmail.com>
2023-05-24 22:18:58 +0200

    Implement expression handle syntax

--- src/reduce.rs
+++ src/reduce.rs
@@ -15,7 +15,7 @@ use crate::{
 };
 
 type RuleFn = dyn Fn(Expr) -> anyhow::Result<Result<Expr, Expr>> + Send + Sync;
-type RuleConst = (Variable, Option<(Variable, Box<[Variable]>)>);
+type RuleMacro = (Variable, Variable, Box<[Variable]>);
 
 pub static RULES: Lazy<Box<[Box<RuleFn>]>> = Lazy::new(parse_rules);
 
@@ -33,32 +33,37 @@ fn parse_rules() -> Box<[Box<RuleFn>]> {
             .map(|caps| (caps.get(1).unwrap().as_str().to_owned(), None))
             .collect();
 
+        let re = regex!("@([a-z])");
+        let tmpl_subexprs: Box<[(Variable, Option<Expr>)]> = re
+            .captures_iter(tmpl_src)
+            .map(|caps| (caps.get(1).unwrap().as_str().to_owned(), None))
+            .collect();
+
+        let re = regex!("(.?)([a-z])");
+        let tmpl_vars: Box<[(Variable, Option<Variable>)]> = re
+            .captures_iter(tmpl_src)
+            .filter(|caps| !matches!(caps.get(1).map(|m| m.as_str()), Some("#" | "@")))
+            .map(|caps| (caps.get(2).unwrap().as_str().to_owned(), None))
+            .collect();
+
+        let re = regex!("(?:#|@)([a-z])");
         let expanded_tmpl_src = re.replace_all(tmpl_src, |caps: &Captures| {
             caps.get(1).unwrap().as_str().to_owned()
         });
 
-        let re = regex!("(.?)([a-z])");
-        let tmpl_vars: Box<[(Variable, Option<Expr>)]> = re
-            .captures_iter(tmpl_src)
-            .filter(|caps| !matches!(caps.get(1).map(|m| m.as_str()), Some("#")))
-            .map(|caps| (caps.get(2).unwrap().as_str().to_owned(), None))
-            .collect();
-
         let tmpl_expr = parse::template_expr(&expanded_tmpl_src).unwrap();
 
-        let re = regex!(r"#([a-z]+)(?:\((?: *([a-z])(?: *, *([a-z]) *)*)?\))?");
-        let mut rule_consts = Vec::<RuleConst>::new();
+        let re = regex!(r"@([a-z])|#([a-z]+)(\((?: *([a-z])(?: *, *([a-z]) *)*)?\))?");
+        let mut rule_consts = Vec::<RuleMacro>::new();
         let mut expanded_rule_src = String::with_capacity(rule_src.len());
         let mut prev_end = 0;
         for caps in re.captures_iter(rule_src) {
             let m = caps.get(0).unwrap();
-            let expr_name = caps.get(1).unwrap().as_str();
+            let expr_name = caps.get(1).or_else(|| caps.get(2)).unwrap().as_str();
 
             expanded_rule_src += &rule_src[prev_end..m.range().start];
             if caps.iter().flatten().count() == 2 {
                 expanded_rule_src += expr_name;
-
-                rule_consts.push((expr_name.to_owned(), None));
             } else {
                 let mut key = format!("{expr_name}{}", UuidFormatter(Uuid::new_v4()));
                 while rule_consts.iter().any(|(k, ..)| k == &key) {
@@ -69,14 +74,12 @@ fn parse_rules() -> Box<[Box<RuleFn>]> {
 
                 rule_consts.push((
                     key,
-                    Some((
-                        expr_name.to_owned(),
-                        caps.iter()
-                            .skip(2)
-                            .flatten()
-                            .map(|m| m.as_str().to_owned())
-                            .collect(),
-                    )),
+                    expr_name.to_owned(),
+                    caps.iter()
+                        .flatten()
+                        .skip(3)
+                        .map(|m| m.as_str().to_owned())
+                        .collect(),
                 ));
             }
 
@@ -90,18 +93,21 @@ fn parse_rules() -> Box<[Box<RuleFn>]> {
         let rule: Box<RuleFn> = Box::new(move |expr| {
             let mut tmpl_consts = tmpl_consts.clone();
             let mut tmpl_vars = tmpl_vars.clone();
+            let mut tmpl_subexprs = tmpl_subexprs.clone();
             if match_template(
                 &expr,
                 &tmpl_expr.expr,
                 tmpl_expr.cond.as_ref(),
                 &mut tmpl_consts,
                 &mut tmpl_vars,
+                &mut tmpl_subexprs,
             )? {
                 Ok(Ok(apply_rule(
                     &rule_expr,
                     &rule_consts,
                     &tmpl_consts,
                     &tmpl_vars,
+                    &tmpl_subexprs,
                 )?))
             } else {
                 Ok(Err(expr))
@@ -118,29 +124,48 @@ fn match_template(
     tmpl_expr: &Expr,
     tmpl_cond: Option<&CmpExpr>,
     tmpl_consts: &mut [(Variable, Option<Number>)],
-    tmpl_vars: &mut [(Variable, Option<Expr>)],
+    tmpl_vars: &mut [(Variable, Option<Variable>)],
+    tmpl_subexprs: &mut [(Variable, Option<Expr>)],
 ) -> anyhow::Result<bool> {
     let did_match = match (expr, tmpl_expr) {
-        (Expr::Literal(Literal::Undefined), Expr::Literal(Literal::Variable(tmpl_var))) => {
-            tmpl_var == "undefined"
+        (Expr::Literal(Literal::Undefined), Expr::Literal(Literal::Variable(tmpl_ident))) => {
+            tmpl_ident == "undefined"
         }
-        (expr, Expr::Literal(Literal::Variable(tmpl_var))) => {
-            if let Some((_, c)) = tmpl_consts.iter_mut().find(|(k, _)| k == tmpl_var) {
+        (expr, Expr::Literal(Literal::Variable(tmpl_ident))) => {
+            if let Some((_, tmpl_num)) = tmpl_consts
+                .iter_mut()
+                .find(|(ident, _)| ident == tmpl_ident)
+            {
                 if let Expr::Literal(Literal::Number(num)) = expr {
-                    if let Some(c) = c.as_ref() {
-                        c == num
+                    if let Some(tmpl_num) = tmpl_num.as_ref() {
+                        tmpl_num == num
                     } else {
-                        *c = Some(num.clone());
+                        *tmpl_num = Some(num.clone());
                         true
                     }
                 } else {
                     false
                 }
-            } else if let Some((_, e)) = tmpl_vars.iter_mut().find(|(k, _)| k == tmpl_var) {
-                if let Some(e) = e.as_ref() {
-                    e == expr
+            } else if let Some((_, tmpl_var)) =
+                tmpl_vars.iter_mut().find(|(ident, _)| ident == tmpl_ident)
+            {
+                if let Expr::Literal(Literal::Variable(var)) = expr {
+                    if let Some(tmpl_var) = tmpl_var.as_ref() {
+                        tmpl_var == var
+                    } else {
+                        *tmpl_var = Some(var.clone());
+                        true
+                    }
                 } else {
-                    *e = Some(expr.clone());
+                    false
+                }
+            } else if let Some((_, tmpl_subexprs)) =
+                tmpl_subexprs.iter_mut().find(|(k, _)| k == tmpl_ident)
+            {
+                if let Some(tmpl_subexprs) = tmpl_subexprs.as_ref() {
+                    tmpl_subexprs == expr
+                } else {
+                    *tmpl_subexprs = Some(expr.clone());
                     true
                 }
             } else {
@@ -154,11 +179,25 @@ fn match_template(
                 None,
                 tmpl_consts,
                 tmpl_vars,
+                tmpl_subexprs,
             )?
         }
         (Expr::BinOp(expr), Expr::BinOp(tmpl_expr)) if expr.bin_op == tmpl_expr.bin_op => {
-            match_template(&expr.lhs, &tmpl_expr.lhs, None, tmpl_consts, tmpl_vars)?
-                && match_template(&expr.rhs, &tmpl_expr.rhs, None, tmpl_consts, tmpl_vars)?
+            match_template(
+                &expr.lhs,
+                &tmpl_expr.lhs,
+                None,
+                tmpl_consts,
+                tmpl_vars,
+                tmpl_subexprs,
+            )? && match_template(
+                &expr.rhs,
+                &tmpl_expr.rhs,
+                None,
+                tmpl_consts,
+                tmpl_vars,
+                tmpl_subexprs,
+            )?
         }
         _ => expr == tmpl_expr,
     };
@@ -191,68 +230,67 @@ fn match_template(
 
 fn apply_rule(
     rule_expr: &Expr,
-    rule_consts: &[RuleConst],
+    rule_consts: &[RuleMacro],
     tmpl_consts: &[(Variable, Option<Number>)],
-    tmpl_vars: &[(Variable, Option<Expr>)],
+    tmpl_vars: &[(Variable, Option<Variable>)],
+    tmpl_subexprs: &[(Variable, Option<Expr>)],
 ) -> anyhow::Result<Expr> {
     let expr = match rule_expr {
         Expr::Literal(Literal::Variable(rule_var)) if rule_var == "undefined" => {
             Expr::Literal(Literal::Undefined)
         }
         Expr::Literal(Literal::Variable(rule_var)) => {
-            if let Some((_, const_expr)) = rule_consts.iter().find(|(k, ..)| k == rule_var) {
-                if let Some((const_expr, args)) = const_expr {
-                    match (&**const_expr, &**args) {
-                        ("sum", [a, b]) => Expr::Literal(Literal::Number(
-                            get_const(a, tmpl_consts) + get_const(b, tmpl_consts),
-                        )),
-                        ("difference", [a, b]) => Expr::Literal(Literal::Number(
-                            get_const(a, tmpl_consts) - get_const(b, tmpl_consts),
-                        )),
-                        ("product", [a, b]) => Expr::Literal(Literal::Number(
-                            get_const(a, tmpl_consts) * get_const(b, tmpl_consts),
-                        )),
-                        ("pow", [a, b]) => {
-                            let b = get_const(b, tmpl_consts);
-                            ensure!(b.bits() <= 32, "exponent too large: {}", b);
-                            let b = b.iter_u32_digits().next().unwrap_or(0);
+            if let Some((_, const_expr, args)) = rule_consts.iter().find(|(k, ..)| k == rule_var) {
+                match (&**const_expr, &**args) {
+                    ("sum", [a, b]) => Expr::Literal(Literal::Number(
+                        get_const(a, tmpl_consts) + get_const(b, tmpl_consts),
+                    )),
+                    ("difference", [a, b]) => Expr::Literal(Literal::Number(
+                        get_const(a, tmpl_consts) - get_const(b, tmpl_consts),
+                    )),
+                    ("product", [a, b]) => Expr::Literal(Literal::Number(
+                        get_const(a, tmpl_consts) * get_const(b, tmpl_consts),
+                    )),
+                    ("pow", [a, b]) => {
+                        let b = get_const(b, tmpl_consts);
+                        ensure!(b.bits() <= 32, "exponent too large: {}", b);
+                        let b = b.iter_u32_digits().next().unwrap_or(0);
 
-                            Expr::Literal(Literal::Number(get_const(a, tmpl_consts).pow(b)))
-                        }
-                        ("negate", [a]) => {
-                            Expr::Literal(Literal::Number(-get_const(a, tmpl_consts)))
-                        }
-                        ("factorial", [a]) => {
-                            let a = get_const(a, tmpl_consts);
-
-                            let two = Number::from(2);
-                            let n = match a.cmp(&two) {
-                                Ordering::Less => Number::one(),
-                                Ordering::Equal => a.clone(),
-                                Ordering::Greater => {
-                                    let mut a = a.clone();
-                                    let mut n = Number::one();
-                                    while a > two {
-                                        n *= &a;
-                                        a -= Number::one();
-                                    }
-                                    n * a
-                                }
-                            };
-
-                            Expr::Literal(Literal::Number(n))
-                        }
-                        (const_expr, args) => panic!(
-                            "unknown constant expression '{const_expr}' with {} argument{}",
-                            args.len(),
-                            if args.len() != 1 { "s" } else { "" }
-                        ),
+                        Expr::Literal(Literal::Number(get_const(a, tmpl_consts).pow(b)))
                     }
-                } else {
-                    Expr::Literal(Literal::Number(get_const(rule_var, tmpl_consts).clone()))
+                    ("negate", [a]) => Expr::Literal(Literal::Number(-get_const(a, tmpl_consts))),
+                    ("factorial", [a]) => {
+                        let a = get_const(a, tmpl_consts);
+
+                        let two = Number::from(2);
+                        let n = match a.cmp(&two) {
+                            Ordering::Less => Number::one(),
+                            Ordering::Equal => a.clone(),
+                            Ordering::Greater => {
+                                let mut a = a.clone();
+                                let mut n = Number::one();
+                                while a > two {
+                                    n *= &a;
+                                    a -= Number::one();
+                                }
+                                n * a
+                            }
+                        };
+
+                        Expr::Literal(Literal::Number(n))
+                    }
+                    (const_expr, args) => panic!(
+                        "unknown constant expression '{const_expr}' with {} argument{}",
+                        args.len(),
+                        if args.len() != 1 { "s" } else { "" }
+                    ),
                 }
+            } else if let Some((_, tmpl_const)) = tmpl_consts.iter().find(|(k, _)| k == rule_var) {
+                Expr::Literal(Literal::Number(tmpl_const.clone().unwrap()))
+            } else if let Some((_, tmpl_var)) = tmpl_vars.iter().find(|(k, _)| k == rule_var) {
+                Expr::Literal(Literal::Variable(tmpl_var.clone().unwrap()))
             } else {
-                let (_, e) = tmpl_vars
+                let (_, e) = tmpl_subexprs
                     .iter()
                     .find(|(k, _)| k == rule_var)
                     .unwrap_or_else(|| panic!("{rule_var} not defined"));
@@ -267,12 +305,25 @@ fn apply_rule(
                 rule_consts,
                 tmpl_consts,
                 tmpl_vars,
+                tmpl_subexprs,
             )?),
         }),
         Expr::BinOp(expr) => Expr::BinOp(ExprBinOp {
             bin_op: expr.bin_op,
-            lhs: Box::new(apply_rule(&expr.lhs, rule_consts, tmpl_consts, tmpl_vars)?),
-            rhs: Box::new(apply_rule(&expr.rhs, rule_consts, tmpl_consts, tmpl_vars)?),
+            lhs: Box::new(apply_rule(
+                &expr.lhs,
+                rule_consts,
+                tmpl_consts,
+                tmpl_vars,
+                tmpl_subexprs,
+            )?),
+            rhs: Box::new(apply_rule(
+                &expr.rhs,
+                rule_consts,
+                tmpl_consts,
+                tmpl_vars,
+                tmpl_subexprs,
+            )?),
         }),
     };
 
